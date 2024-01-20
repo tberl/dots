b7dad37354818ad4e9b5a5ec362f91f11523f9ef
tberl <tberl@users.noreply.github.com>
2024-01-20 21:32:37 +0100

    Added Zsh config

--- install.sh
+++ install.sh
@@ -16,3 +16,7 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
